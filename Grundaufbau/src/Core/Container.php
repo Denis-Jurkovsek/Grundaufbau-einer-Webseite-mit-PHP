29eca1886ff0239f3881c6ae3204d9268681902b
Denis-Jurkovsek <32794909+Denis-Jurkovsek@users.noreply.github.com>
2018-12-19 11:04:50 +0100

    Add files via upload

--- Grundaufbau/src/Core/Container.php
+++ Grundaufbau/src/Core/Container.php
@@ -4,13 +4,6 @@ namespace App\Core;
 
 use PDO;
 use PDOException;
-use App\Post\PostsRepository;
-use App\Post\PostsController;
-use App\Post\CommentsRepository;
-use App\Login\LoginRepository;
-use App\Login\LoginController;
-use App\Login\LoginService;
-use App\Post\AdminController;
 
 class Container
 {
@@ -20,37 +13,7 @@ class Container
     public function __construct()
     {
       $this->receipts = [
-        'postsController' => function() {
-          return new PostsController(
-            $this->make('postsRepository'),
-            $this->make('commentsRepository')
-          );
-        }, 'adminController' => function() {
-          return new AdminController(
-            $this->make('postsRepository'),
-            $this->make('loginService')
-          );
-        }, 'loginService' => function() {
-          return new LoginService(
-            $this->make("loginRepository")
-          );
-        }, 'loginController' => function() {
-          return new LoginController(
-            $this->make("loginService")
-          );
-        }, 'postsRepository' => function() {
-          return new PostsRepository(
-            $this->make("pdo")
-          );
-        }, 'commentsRepository' => function() {
-          return new CommentsRepository(
-            $this->make("pdo")
-          );
-        }, 'loginRepository' => function() {
-          return new LoginRepository(
-            $this->make("pdo")
-          );
-        }, 'pdo' => function() {
+        'pdo' => function() {
           try {
             $pdo = new PDO(
               'mysql:host=HOST;dbname=DBNAME;charset=utf8',
--- Grundaufbau/views/layout/header.php
+++ Grundaufbau/views/layout/header.php
@@ -1,27 +1,16 @@
-<!DOCTYPE html>
 <html lang="de">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Startseite</title>
+    <title>TITLE</title>
 
-    <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-    <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-   <!--[if lt IE 9]>
-     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-   <![endif]-->
   </head>
 
   <body>
@@ -35,11 +24,11 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index">Blog</a>
+          <a class="navbar-brand" href="index">1</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="index">Home</a></li>
+            <li class="active"><a href="index">2</a></li>
           </ul>
         </div>
       </div>
